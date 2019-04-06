af93edb9dbab4a8bf38c3f35b6d9863950564630
Frederik Krautwald <fkrautwald@gmail.com>
2019-04-06 14:02:34 +0200

    Apply linting to source code
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,16 @@ import { loop, map } from '@most/core'
 import { Stream } from '@most/types'
 
 // Replace event values with a 1-based count.
-export const count = <A>(sa: Stream<A>): Stream<number> =>
-  keepIndex(withCount(sa))
+export const count = <A>(sa: Stream<A>): Stream<number> => keepIndex(withCount(sa))
 
 // Pair event values with their associated 1-based count.
-export const withCount = <A>(sa: Stream<A>): Stream<[number, A]> =>
-  withIndexStart(1, sa)
+export const withCount = <A>(sa: Stream<A>): Stream<[number, A]> => withIndexStart(1, sa)
 
 // Replace event values with a 0-based index.
-export const index = <A>(sa: Stream<A>): Stream<number> =>
-  keepIndex(withIndex(sa))
+export const index = <A>(sa: Stream<A>): Stream<number> => keepIndex(withIndex(sa))
 
 // Pair event values with their associated 0-based index.
-export const withIndex = <A>(sa: Stream<A>): Stream<[number, A]> =>
-  withIndexStart(0, sa)
+export const withIndex = <A>(sa: Stream<A>): Stream<[number, A]> => withIndexStart(0, sa)
 
 // Pair event values with their associated `start`-based index.
 export const withIndexStart = <A>(start: number, sa: Stream<A>): Stream<[number, A]> =>
@@ -23,11 +19,14 @@ export const withIndexStart = <A>(start: number, sa: Stream<A>): Stream<[number,
 
 // Pair event values with an iterative index
 export const indexed = <S, I, A>(f: (s: S) => [I, S], init: S, sa: Stream<A>): Stream<[I, A]> =>
-  loop((s, a) => {
-    const [index, seed] = f(s)
-    return { seed, value: [index, a] }
-  }, init, sa)
+  loop(
+    (s, a) => {
+      const [i, seed] = f(s)
+      return { seed, value: [i, a] }
+    },
+    init,
+    sa,
+  )
 
 // Given an indexed Stream, keep only the index and discard the event values.
-export const keepIndex = <I>(s: Stream<[I, unknown]>): Stream<I> =>
-  map(ia => ia[0], s)
+export const keepIndex = <I>(s: Stream<[I, unknown]>): Stream<I> => map(ia => ia[0], s)
